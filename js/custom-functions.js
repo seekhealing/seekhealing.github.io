d8eda6bd2634292b4f45333a63ace0a12cdada9f
Joshua "jag" Ginsberg <jag@flowtheory.net>
2017-05-03 17:51:27 -0400

    Port of Lucy’s fine work to a Jekyll site.

---
+++ js/custom-functions.js
@@ -0,0 +1,31 @@
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('#standardNarrative');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
